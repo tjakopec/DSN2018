85243ba54795ad58f84faf3a595c4079cc6c3a14
Tomislav Jakopec <tjakopec@gmail.com>
2018-09-15 11:48:50 +0200

    Rad na frontendu

--- web/js/app.js
+++ web/js/app.js
@@ -1,17 +1,65 @@
 
 
- 
- dweetio.dweet_for("dsn2018", {"vrata": "otvoreno","prozor": "zatvoreno"});
- 
- /*
- 	dweetio.get_latest_dweet_for("dsn2018", function(err, dweet){
-	
-	if(dweet!=null){
-      var dweet = dweet[0];
-      console.log(dweet); 
-  }  
-   
+var podaci={klima: "0", rasvjeta: "0", grijanje: "0"};
+var dweetId="dsn2018";
+ucitajAPI();
+postaviOznakeCelija();
+
+document.body.onclick= function(e){
+	e=window.event? event.srcElement: e.target;
+	//console.log(e);
+	if(e.nodeName==="IMG" || e.className && e.className.indexOf('celija')!=-1){
+		celijaKlik(e.nodeName==="IMG" ? e.parentNode : e);
+	}
+		
+ }
+
+
+
+ function celijaKlik(e){
+	 var kontrola = e.className.replace("celija ","");
+	 postaviOznakuUcitanje(kontrola);
+	 switch(kontrola){
+		 case "klima":
+		 	podaci.klima = podaci.klima=="0" ? "1" : "0";
+			 break;
+		case "rasvjeta":
+		 	podaci.rasvjeta = podaci.rasvjeta=="0" ? "1" : "0";
+			 break;
+		case "grijanje":
+		 	podaci.grijanje = podaci.grijanje=="0" ? "1" : "0";
+		 	break;
+
+		 default:
+		 	break;
+	 }
+	 odradiAPI();
+	 postaviOznakeCelija();
+ }
+
+ function postaviOznakeCelija(){
+	document.getElementById("klima").src="img/klima_" + podaci.klima  + ".svg";
+	document.getElementById("rasvjeta").src="img/rasvjeta_" + podaci.rasvjeta  + ".svg";
+	document.getElementById("grijanje").src="img/grijanje_" + podaci.grijanje  + ".svg";
+}
+
+ function postaviOznakuUcitanje(elementId){
+	document.getElementById(elementId).src="img/ucitavam.svg";
+}
+
+function odradiAPI(){
+	console.log(podaci);
+	dweetio.dweet_for(dweetId, podaci);
+	ucitajAPI();
+}
+
+function ucitajAPI(){
+	dweetio.get_latest_dweet_for(dweetId, function(err, dweet){
+		if(dweet!=null){
+			podaci=dweet[0].content;
+			console.log(podaci);
+			postaviOznakeCelija();
+		}  
 	});
-	 
-	
-	 */
+}
+ 
